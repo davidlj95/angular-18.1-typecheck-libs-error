90f66def2fae28ff06d78a5fbe21e7805e6b28f2
David LJ <mail@davidlj95.com>
2024-07-25 17:43:16 +0200

    feat: add example lib
    ng g lib example

---
+++ projects/example/src/lib/example.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-example',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      example works!
+    </p>
+  `,
+  styles: ``
+})
+export class ExampleComponent {
+
+}
